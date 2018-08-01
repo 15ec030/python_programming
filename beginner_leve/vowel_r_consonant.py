f13818c9787b59d33c1c2b2d05dc196cda2fa46e
15ec030 <mehanas254@gmail.com>
2018-08-01 16:07:11 +0530

    Update vowel_r_consonant.py

--- beginner_leve/vowel_r_consonant.py
+++ beginner_leve/vowel_r_consonant.py
@@ -2,5 +2,8 @@ alphabet="a,e,i,o,u"
 n=input()
 if(n in alphabet):
     print("vowel")
-else:
+con="b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z"    
+if (n in con):
     print("consonant")
+else:
+    print ("invalid")
